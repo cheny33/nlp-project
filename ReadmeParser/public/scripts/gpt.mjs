e689783dfee94005112d73889a50d2f61c22922c
Khai Phung <phunga@rose-hulman.edu>
2023-05-11 15:31:27 -0400

    add api

--- ReadmeParser/public/scripts/gpt.mjs
+++ ReadmeParser/public/scripts/gpt.mjs
@@ -5,16 +5,32 @@ import {
 
 
 
-    //const { Configuration, OpenAIApi } = require("openai");
-    const configuration = new Configuration({
-        organization: "org-7iwDAUhp2gK0kJaJmN67GZGf",
-        apiKey: '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',
-    });
-    const openai = new OpenAIApi(configuration);
+//const { Configuration, OpenAIApi } = require("openai");
+// const configuration = new Configuration({
+//     organization: "org-7iwDAUhp2gK0kJaJmN67GZGf",
+//     apiKey: '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',
+// });
+// const openai = new OpenAIApi(configuration);
+// const completion = await openai.createChatCompletion({
+//     model: "gpt-3.5-turbo",
+//     messages: [{
+//         role: "user",
+//         content: "What is the weather in Terre Haute 2 years ago"
+//     }],
+// });
+
+// const { Configuration, OpenAIApi } = require("openai");
+// const configuration = new Configuration({
+//   apiKey: process.env.OPENAI_API_KEY,
+// });
+// const openai = new OpenAIApi(configuration);
+// const response = await openai.listModels();
+
+
+console.log(completion.data.choices[0].message.content);
+
+function config (){
     
-    const completion = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      messages: [{role: "user", content: "What is the weather in Terre Haute 2 years ago"}],
-    });
-    console.log(completion.data.choices[0].message.content);
-    
+}
+
+export const foo = completion.data.choices[0].message.content;
